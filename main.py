b993ed987aff0d1d055f0a1f0eb6df2ed2b8d8d9
Wizock <Rohaanerodasahmed@gmail.com>
2020-10-24 19:25:01 +1000

    refresh

--- main.py
+++ main.py
@@ -1,12 +1,2 @@
-import sqlite3
-from sqlite3 import Error
-import os
-from DATABASE import 
+from api import *
 
-connect_req = connecttodatabase.connectDB()
-
-createTable = createTable.create_table()
-
-signin = signin()
-
-signup = sign_up()
