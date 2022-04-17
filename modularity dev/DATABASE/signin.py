820b4505b1f467e032eb4f3cda65afc1f60c4823
Ishchild ApartmentAngels <36351054+Wizock@users.noreply.github.com>
2022-04-18 01:26:28 +1000

    Update signin.py

--- modularity dev/DATABASE/signin.py
+++ modularity dev/DATABASE/signin.py
@@ -9,17 +9,26 @@ class signin:
     def login(self):
 
         self.con.execute("SELECT userName, password FROM users")
-        if self.login_Password not in self.fetchAuthData or self.login_Username not in self.fetchAuthData:
-            for i in range(0,6):
+        if (
+            self.login_Password not in self.fetchAuthData
+            or self.login_Username not in self.fetchAuthData
+        ):
+            for i in range(0, 6):
                 print("please try again")
                 self.login_Username = input("enter your username:")
                 self.login_Password = input("enter your password:")
-                if self.login_Password not in self.fetchAuthData or self.login_Username not in self.fetchAuthData:
+                if (
+                    self.login_Password not in self.fetchAuthData
+                    or self.login_Username not in self.fetchAuthData
+                ):
                     print("login sucessful")
                     break
                 else:
                     if i >= 5:
                         print("please try again later")
                         break
-        if self.login_Password in self.fetchAuthData and self.login_Username in self.fetchAuthData:
-            print("Login Sucessful")            
+        if (
+            self.login_Password in self.fetchAuthData
+            and self.login_Username in self.fetchAuthData
+        ):
+            print("Login Sucessful")
