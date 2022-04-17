3187620e251b243fdcf8dc7acc994080987498f0
Ishchild ApartmentAngels <36351054+Wizock@users.noreply.github.com>
2022-04-18 01:22:22 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
 from api import *
 from admin import *
-import sqlite3
+import sqlite3, spriteClicked
+
 
 def main():
 
     login_page()
-    
+
     while True:
         pygame.event.pump()
-        #functions
-        if keyPressed('space'):
+        # functions
+        if keyPressed("space"):
             end()
         if spriteClicked(editTableBTN):
-            print("you want to edit the database")    
+            print("you want to edit the database")
         if spriteClicked(registerBTN):
             print("you want to register a employee")
         if spriteClicked(viewTBBTN):
             print("you want to view the database tables")
-        if spriteClicked
-        
-
     endWait()
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     main()
-
-
-
